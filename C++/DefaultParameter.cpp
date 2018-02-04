8a00d4f5ee5f8e026330ded88ed17cef1ecd7f35
Yugi62 <30305369+Yugi62@users.noreply.github.com>
2018-02-04 13:40:53 +0900

    Revise) C++->DefaultParameter

--- C++/DefaultParameter.cpp
+++ C++/DefaultParameter.cpp
@@ -10,7 +10,7 @@ using namespace std;
 디폴트 매개변수 : 인자를 전달받지 못했을 때 그 값을 대신해주기 위해 미리 설정되어 있는 변수
 */
 
-void Display(int x = 0)
+void Display(int x = 0)	//매개변수에 값을 대입하면 디폴트 매개변수가 된다
 {
 	cout << x << endl;
 }
