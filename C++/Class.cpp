0fd106473643364cbc06e332db48b560a60d01db
Yugi62 <30305369+Yugi62@users.noreply.github.com>
2018-02-04 16:24:48 +0900

    Revise) C++->Class

--- C++/Class.cpp
+++ C++/Class.cpp
@@ -46,7 +46,7 @@ public:					//접근 지시자 선언 (다른 접근 지시자 선언까지 'public')
 	void Display2();		//멤버 함수 선언
 };
 
-void Point::Display2()		//외부에서 생성자 정의
+void Point::Display2()			//외부에서 생성자 정의
 {
 	cout << x << endl;		//내부에서 'private'인 x 접근 
 	cout << y << endl;		//내부에서 'private'인 y 접근
@@ -56,8 +56,8 @@ int main()
 {
 	Point p1;		//p1 인스턴스 선언
 
-	//p1.x = 10;	외부에서 'private'인 x 접근 불가
-	//p1.y = 20;	외부에서 'private'인 y 접근 불가
+	//p1.x = 10;			외부에서 'private'인 x 접근 불가
+	//p1.y = 20;			외부에서 'private'인 y 접근 불가
 
 
 	p1.Display1();		//외부에서 '.'으로 'public' Display1() 멤버 함수 접근
